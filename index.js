f0ed0160863098f526bf6e6562d40e2c33411f38
Deepak Singh <dksingh7367828168@gmail.com>
2022-04-03 23:03:04 +0530

    first

---
+++ index.js
@@ -0,0 +1,84 @@
+ /*
+ // javascript console api
+        // ways to print in javascript
+        console.log("Hello world")
+        console.error("this is an error")
+        console.worn("this is warning")
+        // alert("me");
+        //    docment.write("this is docment write")
+
+        console.worn("this is wrining")
+        consol.worning("this is a wrinning")
+        console.error("This is error");
+        console.clear()
+        console.assert(4 == 8)
+        comsole.assert(4 - 6)
+        console.log(4 + 6)
+        */
+        // variable is a container in javascript
+        var number1=7;
+        var number2=9;
+        // console.log(number1 + number2);
+        // 4 data type in javascript
+        var str1="this is a string";
+        var str2='this is also string'
+        // number
+        var num1=2;
+        var num2=45;
+        // object
+        var marks ={
+            ravi: 12,
+            subham: 67,
+            depak:99.99
+        }
+        // console.log(marks)
+        // booleans 
+        var a=true;
+        let b=false;
+        // console.log(a,b)
+        var und = undefined;
+        console.log(und);
+        var n=null;
+        //  console.log(n)
+         /*
+        At a very high level, there are two types of data types in javascript
+        1. primitive:undefined,null,numbers ,strings,boolean,symbols
+        2. Refrance data type: Arrays or objects
+        */
+
+        // Arrays
+        var arr =[1,2,3,5,"dk",6];
+        // console.log(arr);
+
+        // operators in javascript
+        let x=10;
+        let y=100;
+        /*
+        console.log("The valus of x+y is",x+y)
+        console.log("The valus of x-y is",x-y)
+        console.log("The valus of x*y is",x*y)
+        console.log("The valus of x/y is",x/y)
+        */
+        // Assignment op
+        var c=x;
+        c*=y
+        // console.log(c)
+        // comparision op
+        var p=34;
+        var q=78;
+        
+        // console.log(p==y)
+        // console.log(p>q);
+        // console.log(p<q);
+        // console.log(p>=q);
+        // console.log(p<=q);
+        
+        // logical op
+        console.log(true && true);
+        console.log(true && false);
+        console.log(false && false);
+        console.log(false && true);
+
+
+        
+
